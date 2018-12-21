d31baf59f0f842417b83f703aee639a3a991104b
mbutler <matthewtbutler@gmail.com>
2018-12-20 19:37:50 -0600

    move functions to user.js

--- src/session.js
+++ src/session.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 import * as Game from './game'
+import * as User from './user'
 
 export function init(user) {
   let page = location.href.split("/").slice(-1)
@@ -12,6 +13,10 @@ export function init(user) {
 
   if (page[0] === 'character.html') {
     displayCharacterSheet(user)
+  }
+
+  if (page[0] === 'newcharacter.html') {
+    displayNewCharacter(user)
   }
 }
 
@@ -24,7 +29,7 @@ function displayGame(user) {
         let game = data.val()
         $('.game-title').text(game.metadata.title)      
 
-        _.forEach(getUserCharacters(game), player => {
+        _.forEach(User.getUserCharacters(game), player => {
           let link = ''                  
           if (user.uid === player.userId) {
             link = `<a href="character.html">${player.characterName}</a>`
@@ -45,46 +50,23 @@ function displayCharacterSheet(user) {
       gameRef.on("value", data => {
         let game = data.val()
         $('.game-title').text(game.metadata.title)        
-        let character = getCharacterSheet(user, game)
+        let character = User.getCharacterSheet(user, game)
         $('#character-name').append(`<h2>${character.characterName}</h2>`)
       })
   })
 }
 
-function getUserCharacters(game) {
-  let list = []
-  let users = game.users
-  let characters = game.content.characters
-  let characterIdList = []
-  _.forEach(characters, c => {characterIdList.push(c.user)})
-  _.forEach(users, u => {
-    let userId = _.findKey(users, o => { return o === u })
-    if (_.includes(characterIdList, userId ) === false) {
-      let guy = {}    
-      guy.userId = userId
-      guy.characterId = ''
-      guy.characterName = '[empty slot]'
-      guy.userName = users[userId]
-      guy.gameId = game.metadata.gameId
-      list.push(guy)
-    }
+function displayNewCharacter(user) {
+  let ref = firebase.database().ref("users/" + user.uid + "/currentGame")
+  ref.on("value", snapshot => {
+      let gameId = snapshot.val()
+      let gameRef = firebase.database().ref("users/" + user.uid + "/games/" + gameId)
+      gameRef.on("value", data => {
+        let game = data.val()
+        $('.game-title').text(game.metadata.title)        
+        let character = User.getCharacterSheet(user, game)
+        $('#character-name').append(`<h2>${character.characterName}</h2>`)
+      })
   })
-
-  _.forEach(characters, character => {
-    let guy = {}    
-    guy.userId = character.user 
-    guy.characterId = _.findKey(characters, o => { return o.name === character.name })
-    guy.characterName = character.name
-    guy.userName = users[character.user]
-    guy.gameId = game.metadata.gameId
-    list.push(guy)
-  })
-
-  return list
 }
 
-function getCharacterSheet(user, game) {
-  let userCharacters = getUserCharacters(game)
-  let character = _.find(userCharacters, player => {return player.userId === user.uid})
-  return character
-}
--- src/user.js
+++ src/user.js
@@ -17,6 +17,44 @@ export function addNew(user) {
   })
 }
 
+export function getUserCharacters(game) {
+  let list = []
+  let users = game.users
+  let characters = game.content.characters
+  let characterIdList = []
+  _.forEach(characters, c => {characterIdList.push(c.user)})
+  _.forEach(users, u => {
+    let userId = _.findKey(users, o => { return o === u })
+    if (_.includes(characterIdList, userId ) === false) {
+      let guy = {}    
+      guy.userId = userId
+      guy.characterId = ''
+      guy.characterName = '[empty slot]'
+      guy.userName = users[userId]
+      guy.gameId = game.metadata.gameId
+      list.push(guy)
+    }
+  })
+
+  _.forEach(characters, character => {
+    let guy = {}    
+    guy.userId = character.user 
+    guy.characterId = _.findKey(characters, o => { return o.name === character.name })
+    guy.characterName = character.name
+    guy.userName = users[character.user]
+    guy.gameId = game.metadata.gameId
+    list.push(guy)
+  })
+
+  return list
+}
+
+export function getCharacterSheet(user, game) {
+  let userCharacters = getUserCharacters(game)
+  let character = _.find(userCharacters, player => {return player.userId === user.uid})
+  return character
+}
+
 export async function ids() {
   let userids = firebase.database().ref('userIds/IgwAYKuzhIPMkhcrNIuO23GE5yr2').once('value')
   let value = await Promise.all([userids])
